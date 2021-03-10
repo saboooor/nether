4653c72701e832481a0f46e5e031b16c02b55807
Saboor <saboor547@gmail.com>
2021-03-09 23:43:58 -0700

    responsive

---
+++ beta/assets/script.js
@@ -0,0 +1,8 @@
+function navExpand() {
+    var x = document.getElementById("navigation");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
