abb88f11c71b346fdd3cf62ea05cc92da18eab66
Saboor <saboor547@gmail.com>
2021-03-10 03:15:57 -0700

    fix blur for ios and make fullscreen

--- beta/assets/script.js
+++ beta/assets/script.js
@@ -1,9 +1,7 @@
 function openNav() {
-  document.getElementById("mobilenav").style.height = "50%";
-  document.getElementById("mobilenavbackground").style.height = "50%";
+  document.getElementById("mobilenav").style.height = "100%";
 }
 
 function closeNav() {
   document.getElementById("mobilenav").style.height = "0%";
-  document.getElementById("mobilenavbackground").style.height = "0%";
 }
