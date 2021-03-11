38e5d835a24d3dacbaa3fac4cfc14901b5ea546f
Saboor <saboor547@gmail.com>
2021-03-11 07:51:15 -0700

    this looks better

--- new/assets/script.js
+++ new/assets/script.js
@@ -1,9 +1,14 @@
-function openNav() {
-  document.getElementById("mobilenav").style.height = "100%";
-  document.getElementById("dropbtn").style.display = "none";
-}
-
-function closeNav() {
-  document.getElementById("mobilenav").style.height = "0%";
-  document.getElementById("dropbtn").style.display = null;
+function toggleNav() {
+  if (document.getElementById("mobilenav").style.height != "100%") {
+    document.getElementById("mobilenav").style.height = "100%";
+    document.getElementById("mobilenav").style.opacity = "100%";
+    document.getElementById("mobilenav").style.paddingTop = "50px";
+    document.getElementById("navigation").style.backgroundColor = 'rgba(0,0,0, 0.8)';
+  }
+  else {
+    document.getElementById("mobilenav").style.height = "0";
+    document.getElementById("mobilenav").style.opacity = "0";
+    document.getElementById("mobilenav").style.paddingTop = "0";
+    document.getElementById("navigation").style.backgroundColor = null;
+  }
 }
