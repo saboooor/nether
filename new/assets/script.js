ec12754e0c2eae954757ba73bc7fc312e76931db
Saboor <saboor547@gmail.com>
2021-03-11 02:42:00 -0700

    padding stuff and make the menu button better

--- new/assets/script.js
+++ new/assets/script.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("mobilenav").style.height = "100%";
+  document.getElementById("dropbtn").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("mobilenav").style.height = "0%";
+  document.getElementById("dropbtn").style.display = null;
 }
