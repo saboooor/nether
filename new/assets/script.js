728205ab74059bd212cb44d95a413c7fb0d3136b
Saboor <saboor547@gmail.com>
2021-03-13 03:32:09 -0700

    Add border to other submenu, Main title, Copiable IP field

--- new/assets/script.js
+++ new/assets/script.js
@@ -12,3 +12,26 @@ function toggleNav() {
     document.getElementById("navigation").style.backgroundColor = null;
   }
 }
+
+function sleep(ms) {
+	return new Promise(res => setTimeout(res, ms));
+}
+
+async function copyTextToClipboard(text) {
+  var elem = document.getElementById(text);
+  if (elem.innerText == 'Copied to clipboard!') return;
+  var textArea = document.createElement("textarea");
+  textArea.style.opacity = '0';
+  textArea.value = elem.innerText;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  document.execCommand('copy');
+  elem.innerText = 'Copied to clipboard!';
+  await sleep('1000');
+  elem.innerText = textArea.value;
+
+  document.body.removeChild(textArea);
+}
